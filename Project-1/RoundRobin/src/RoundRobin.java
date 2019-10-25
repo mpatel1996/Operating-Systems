da92b43c758a346c2da3279213b2d3d85754cfb8
Mihir Patel <mpjv12343@gmail.com>
2019-10-24 19:41:39 -0700

    Updated the comments.
    changed Trail counter to K + 1. No more creating additional var for trail #.

--- Project-1/RoundRobin/src/RoundRobin.java
+++ Project-1/RoundRobin/src/RoundRobin.java
@@ -17,11 +17,13 @@ public class RoundRobin {
     // changes the quantum time between jobs
     public static int quantum = 5;
     
-    // picking which jobs to take. 5,10,15 jobs file
+    // Addon variable is used for try method when looking for a file inside a folder. 
+    // Explain in line 41.
     public static int addon = 5; 
     public static int overallTotal = 0;
+    
     public static void main(String [] argv) throws IOException {
-        int counter = 1;
+        
         ArrayList<Integer> id = new ArrayList<>(); // Wait Time
         ArrayList<Integer> wTime = new ArrayList<>(); // Wait Time
         ArrayList<Integer> bTime = new ArrayList<>(); // job time
@@ -29,10 +31,14 @@ public class RoundRobin {
         int total = 0;
         float avg;
         
+        // Try to save file to folder. Continue if successfull.
         try (FileWriter fw = new FileWriter("../jobs/output_RR"+ quantum+ "_" + addon + ".txt")) {
+            
+            // Loop until all 10 trials for specific job is finished.
             for(int k = 0; k <10; k++){
-                fw.write("Trial #: " + counter + "\tQuantum #: " + quantum);
-                counter++;
+                
+                // Go to folder jobs, look for file name job_addon_k.txt
+               // Addon is number of the job, and k is number for trail. 
                 File myFile = new File("../jobs/job_"+addon+"_" + k +".txt");
 
                 // Loops until there is next line
@@ -48,6 +54,7 @@ public class RoundRobin {
                     s.close();
                 }
                 
+                // Convert the ArrayList to arrays. 
                 Integer[] wtimearr = new Integer[bTime.size()];
                 Integer[] btimearr = new Integer[bTime.size()];
                 Integer[] idarr = new Integer[bTime.size()];
@@ -57,26 +64,33 @@ public class RoundRobin {
                 btimearr = bTime.toArray(btimearr);
                 idarr = id.toArray(idarr);
                 
+                // Initialize the arrays to 0
                 for (int j =0; j < bTime.size(); j++){
                     idarr[j] = j+1;
                     wtimearr[j] = 0;
                     completearr[j] = 0;
                 }
                 
+                // Call the method for Round Robin Algorithm 
                 roundRobin(wtimearr, btimearr, completearr, total);
-                
+               
+                // Start loop from 1 to length of wtimearr. Because First job to wait is always going to be 0,
+                // start adding the time for job to wait 
                 for(int i = 1; i < wtimearr.length; i++){
                     wtimearr[i]= btimearr[i-1] + wtimearr[i-1] ;                    
                     completearr[i-1] = wtimearr[i-1] + btimearr[i-1];
                     total=total+completearr[i-1];
                 }
                 
+                // Add in the last remaining data since for loop start at 1, not 0. 
                 completearr[wtimearr.length - 1] = wtimearr[wtimearr.length -1] + btimearr[wtimearr.length- 1];
                 total=total+completearr[wtimearr.length-1];
                 
                 int n = bTime.size();
                 avg = (float)total/n;
-                //System.out.println("\nProcess_ID\tBurst_time\tWait_time\tcomplete_time");
+                
+                // Start writing the output to the file. 
+                fw.write("Trial #: " + (k + 1) + "\tQuantum Slice #: " + quantum );
                 fw.write("\nProcess_ID\tBurst_time\tWait_time\tcomplete_time\n");
                 for(int i=0;i<n;i++)
                 {
@@ -87,6 +101,7 @@ public class RoundRobin {
                 //System.out.println("\nTotal wait time: "+total+"\nAverage wait time: "+avg);
                 fw.write("\nTotal time: "+total+"\nAverage Turn around Time: "+avg + "\n\n");
                 
+                // overallTotal is calculating all total average values for 10 trials for specific job. 
                 overallTotal += total;
                 id.clear();
                 wTime.clear();
@@ -101,32 +116,30 @@ public class RoundRobin {
         }
     }// end of main
 
+    // Start of Round Robin algorithm. takes in []wtimearr, []btimearr, []carr, and t integer as total
     private static void roundRobin(Integer[] wtimearr, Integer[] btimearr, 
                                     int[] carr, int t ) {
         
         int n = btimearr.length;
-         int rem_bt[] = new int[n]; 
-        for (int i = 0 ; i < n ; i++) 
+        int rem_bt[] = new int[n]; 
+        
+        for (int i = 0 ; i < n ; i++) {
             rem_bt[i] =  btimearr[i]; 
-       
+        }
        
         // Keep traversing processes in round robin manner 
         // until all of them are not done. 
-        while(true) 
-        { 
+        while(true){ 
             boolean done = true; 
        
             // Traverse all processes one by one repeatedly 
-            for (int i = 0 ; i < n; i++) 
-            { 
+            for (int i = 0 ; i < n; i++){ 
                 // If burst time of a process is greater than 0 
                 // then only need to process further 
-                if (rem_bt[i] > 0) 
-                { 
+                if (rem_bt[i] > 0){ 
                     done = false; // There is a pending process 
        
-                    if (rem_bt[i] > quantum) 
-                    { 
+                    if (rem_bt[i] > quantum){ 
                         // Increase the value of t i.e. shows 
                         // how much time a process has been processed 
                         t += quantum; 
@@ -138,8 +151,7 @@ public class RoundRobin {
        
                     // If burst time is smaller than or equal to 
                     // quantum. Last cycle for this process 
-                    else
-                    { 
+                    else{ 
                         // Increase the value of t i.e. shows 
                         // how much time a process has been processed 
                         t = t + rem_bt[i]; 
@@ -152,12 +164,15 @@ public class RoundRobin {
                         // make its remaining burst time = 0 
                         rem_bt[i] = 0; 
                     } 
+                    // Calculate the completed time for job
                     carr[i] = wtimearr[i] + btimearr[i];
                 } 
-            }
+            } // end of for loop
+            
              if (done == true) 
               break; 
-        }
+        } // end of While Loop
         
-    }
-}
+    } // end of Round Robin
+    
+} // End of Class
