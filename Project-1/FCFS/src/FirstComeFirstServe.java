869d2e1693cd3e91c6d3488410bf30f2bc3f4c14
Mihir Patel <mpjv12343@gmail.com>
2019-10-24 19:21:58 -0700

    Updated comments for the file.
    Also removed the counter var for trail number and used k+1 instead.

--- Project-1/FCFS/src/FirstComeFirstServe.java
+++ Project-1/FCFS/src/FirstComeFirstServe.java
@@ -13,22 +13,31 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class FirstComeFirstServe {
+    
+    // static vars for program to use. 
     public static int overallTotal = 0;
+    
+    // Addon variable is used for try method when looking for a file inside a folder. 
+    // Explain in line 41. 
     public static int addon = 15;
     
     public static void main(String [] argv) throws IOException {
-        int counter = 1;
-        ArrayList<Integer> id = new ArrayList<>(); // Wait Time
-        ArrayList<Integer> wTime = new ArrayList<>(); // Wait Time
-        ArrayList<Integer> bTime = new ArrayList<>(); // job time
+        
+        ArrayList<Integer> id = new ArrayList<>(); // Wait Time ArrayList
+        ArrayList<Integer> wTime = new ArrayList<>(); // Wait Time ArrayList
+        ArrayList<Integer> bTime = new ArrayList<>(); // job time ArrayList
         
         int total = 0;
         float avg;
         
+       // Try to save file to folder. Continue if successfull. 
        try (FileWriter fw = new FileWriter("../jobs/output_FCFS_" + addon +".txt")) {
+           
+            // Loop until all 10 trials for specific job is finished. 
             for(int k = 0; k <10; k++){
-                fw.write("Trial #: " + counter);
-                counter++;
+                
+               // Go to folder jobs, look for file name job_addon_k.txt
+               // Addon is number of the job, and k is number for trail. 
                File myFile = new File("../jobs/job_"+ addon + "_" + k +".txt");
 
                 // Loops until there is next line
@@ -44,6 +53,7 @@ public class FirstComeFirstServe {
                     s.close();
                 }
                 
+                // Convert the ArrayList to arrays. 
                 Integer[] wtimearr = new Integer[bTime.size()];
                 Integer[] btimearr = new Integer[bTime.size()];
                 Integer[] idarr = new Integer[bTime.size()];
@@ -59,18 +69,23 @@ public class FirstComeFirstServe {
                     completearr[j] = 0;
                 }
                 
+                // Start loop from 1 to length of wtimearr. Because First job to wait is always going to be 0,
+                // start adding the time for job to wait 
                 for(int i = 1; i < wtimearr.length; i++){
                     wtimearr[i]= btimearr[i-1] + wtimearr[i-1] ;                    
                     completearr[i-1] = wtimearr[i-1] + btimearr[i-1];
                     total=total+completearr[i-1];
                 }
                 
+                // Add in the last remaining data since for loop start at 1, not 0. 
                 completearr[wtimearr.length - 1] = wtimearr[wtimearr.length -1] + btimearr[wtimearr.length- 1];
                 total=total+completearr[wtimearr.length-1];
                 
                 int n = bTime.size();
                 avg = (float)total/n;
-                //System.out.println("\nProcess_ID\tBurst_time\tWait_time\tcomplete_time");
+                
+                // Start writing the output to the File. 
+                fw.write("Trial #: " + (k + 1));
                 fw.write("\nProcess_ID\tBurst_time\tWait_time\tcomplete_time\n");
                 for(int i=0;i<n;i++)
                 {
@@ -82,7 +97,10 @@ public class FirstComeFirstServe {
                 //System.out.println("\nTotal wait time: "+total+"\nAverage wait time: "+avg);
                 fw.write("\nTotal time: "+total+"\nAverage Turn Around Time: "+avg + "\n\n");
                 
+                // overallTotal is calculating all total average values for 10 trials for specific job. 
                 overallTotal += total;
+                
+                // clear out the arraylist to make room for the new file to add its values to the list. 
                 id.clear();
                 wTime.clear();
                 bTime.clear();
@@ -91,9 +109,14 @@ public class FirstComeFirstServe {
                 avg = 0;
             } // end of for loop
             
+            // Write to file the overall total time and overall average time. useful for part 1c of the write up.
             fw.write("Total of all " + addon+ " total time: " + overallTotal);
             fw.write("\nTotal average of all " + addon+ " time: " + (float)(overallTotal)/10);
+            
+            // close the file writer once done using the file. 
             fw.close();
-        }
+           
+        }// End of try method that saves file to output folder
+        
     }// end of main
-}
+} // end of class
