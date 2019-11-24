b92252db10c0bdf395de5e0a4040722afb58af83
Mihir Patel <mpjv12343@gmail.com>
2019-11-24 00:39:41 -0800

    Update driver.java

--- Project_2/Algorithm/driver.java
+++ Project_2/Algorithm/driver.java
@@ -3,10 +3,13 @@ import java.io.IOException;
 import java.util.*;
 
 class driver {
+   
+   static FileWriter  outPuts;
    public static void main(String[] args) throws IOException {
 
       // Creating a refString file to output all strings 
       FileWriter fw = new FileWriter("referenceString.txt");
+      outPuts = new FileWriter("Faults.txt");
 
       // This will hold the value when stringGen is called later on.
       String currString;
@@ -14,6 +17,7 @@ class driver {
       
       // To hold averages for the 3 algorithm. 
       int[][] average = new int[3][4];
+      int[][] outs = new int[3][4];
 
       // Create 50 strings by and find averages for 3 algo.
       for(int i = 0; i < numberOfStrings; i++){         
@@ -36,6 +40,24 @@ class driver {
          average[2][1] += optimal.pageFaults(currString, 4);
          average[2][2] += optimal.pageFaults(currString, 5);
          average[2][3] += optimal.pageFaults(currString, 6);
+
+         outs[0][0] = fifo.pageFaults(currString, 3);
+         outs[0][1] = fifo.pageFaults(currString, 4);
+         outs[0][2] = fifo.pageFaults(currString, 5);
+         outs[0][3] = fifo.pageFaults(currString, 6);
+
+         outs[1][0] = lru.pageFaults(currString, 3);
+         outs[1][1] = lru.pageFaults(currString, 4);
+         outs[1][2] = lru.pageFaults(currString, 5);
+         outs[1][3] = lru.pageFaults(currString, 6);
+
+         outs[2][0] = optimal.pageFaults(currString, 3);
+         outs[2][1] = optimal.pageFaults(currString, 4);
+         outs[2][2] = optimal.pageFaults(currString, 5);
+         outs[2][3] = optimal.pageFaults(currString, 6);
+
+         outPuts.write("Current String Is: "+ currString);
+         printOuts(outs);
       }
 
       // Loop through all total page Fault and average it up.
@@ -47,6 +69,7 @@ class driver {
 
       // Close FW for referenceString.txt
       fw.close();
+      outPuts.close();
 
       // This prints all averages to file called "Averages.txt"
       printAvrg(average);
@@ -54,7 +77,33 @@ class driver {
    } // END of Main
 
    
-   // Creates a new File called "Averages.txt" and prints all averages to that. 
+   private static void printOuts(int[][] outs) throws IOException {
+      
+      outPuts.write("\nPage Frame Size 3 *");
+      outPuts.write("\n*******************");
+      outPuts.write("\nFIFO average size: " + outs[0][0]);
+      outPuts.write("\nLRU average size: " + outs[1][0]);
+      outPuts.write("\nOPTIMAL average size: " + outs[2][0]);
+      outPuts.write("\n\nPage Frame Size 4 *");
+      outPuts.write("\n*******************");
+      outPuts.write("\nFIFO average size: " + outs[0][1]);
+      outPuts.write("\nLRU average size: " + outs[1][1]);
+      outPuts.write("\nOPTIMAL average size: " + outs[2][1]);
+      outPuts.write("\n\nPage Frame Size 5 *");
+      outPuts.write("\n*******************");
+      outPuts.write("\nFIFO average size: " + outs[0][2]);
+      outPuts.write("\nLRU average size: " + outs[1][2]);
+      outPuts.write("\nOPTIMAL average size: " + outs[2][2]);
+      outPuts.write("\n\nPage Frame Size 6 *");
+      outPuts.write("\n*******************");
+      outPuts.write("\nFIFO average size: " + outs[0][3]);
+      outPuts.write("\nLRU average size: " + outs[1][3]);
+      outPuts.write("\nOPTIMAL average size: " + outs[2][3]);
+      outPuts.write("\n");
+
+   }
+
+   // Creates a new File called "Averages.txt" and prints all averages to that.
    private static void printAvrg(int[][] average) throws IOException {
       FileWriter  outPuWriter = new FileWriter("Averages.txt");
 
